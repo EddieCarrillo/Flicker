e881f5088a64680252201eb041effc9745ffa9f4
my mac <mymac@my-macs-macbook.local>
2017-02-06 22:43:57 -0800

    Add optional features

---
+++ MovieCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by my mac on 2/4/17.
+//  Copyright © 2017 Eduardo Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
